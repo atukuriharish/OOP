f4508f10be0053ac2b429c1eac989440a3e3a287
atukuriharish <90964212+atukuriharish@users.noreply.github.com>
2021-09-18 16:26:23 +0530

    Add files via upload

---
+++ Split the array and add the first part to the end.py
@@ -0,0 +1,17 @@
+def splitArr(arr, n, k): 
+    for i in range(0, k): 
+        x = arr[0]
+        for j in range(0, n-1):
+            arr[j] = arr[j + 1]
+          
+        arr[n-1] = x
+          
+arr = [5,50,6,25,4,9]
+n = len(arr)
+position = 2
+  
+splitArr(arr, n, position)
+  
+for i in range(0, n): 
+    print(arr[i], end = ' ')
+  
---
+++ Substring is Present in a Given String.py
@@ -0,0 +1,10 @@
+def check(string, sub_str):
+    if (string.find(sub_str) == -1):
+        print("NO")
+    else:
+        print("YES")
+            
+
+string = "Haris"
+sub_str ="Hari"
+check(string, sub_str)
